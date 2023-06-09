d6f35a8c51ffd62b29a4197fdcf9ccc21e54f590
Susana Alvarado <130176230+susialvarado@users.noreply.github.com>
2023-06-08 20:10:18 -0600

    Add files via upload

--- rango/js/rango.js
+++ rango/js/rango.js
@@ -1,1 +1,26 @@
+/*
+Reto #2 - En el rango, por favor.
+
+Pide al usuario que indique 2 números: uno que servirá como límite y otro para comparar. Si el segundo número es menor al primero, mostrarás un mensaje diciendo “El número ‘x’ se encuentra en el rango, gracias” y en caso contrario dirá “El número ‘x’ excede el límite permitido”.
+*/
+
+var numLim = 0;
+var numCom = 0;
+var jugadas = 0;
+
+while (jugadas < 3) {
+
+    numLim = prompt("Ingresa un número mayor a 0 que servirá como límite superior: ");
+    numCom = prompt("Ingresa cualquier número: ");
+
+    if (numCom <= numLim) {
+        alert("El número " + numCom + " se encuentra en el rango, gracias.");
+        jugadas = jugadas +1
+    } else {
+        alert("El número " + numCom + " excede el límite permitido.");
+        jugadas = jugadas +1
+    }
+    
+}
+
 
