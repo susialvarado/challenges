0633f1d1839dad8f301c00077453a4fb6a8f764d
Susana Alvarado <130176230+SusAlvarado@users.noreply.github.com>
2023-06-13 11:48:07 -0600

    agregué nuevo código al archivo turtles

---
+++ turtles/js/turtles.js
@@ -0,0 +1,19 @@
+/*
+Reto #4 - “I like turtles”
+Escribe un programa que pida al usuario ingrese su animal favorito, si coloca ‘Tortuga’, ‘tortuga’, ‘TORTUGA’ o cualquier otra variante de la palabra entonces mostrar en pantalla “También me gustan las tortugas”. En caso contrario mostrar el mensaje “Ese animal es genial, pero prefiero las tortugas”.
+*/
+
+var animal = 0;
+var preguntas = 0;
+
+while (preguntas < 3) {
+    
+    animal = prompt("¿Cuál es tu animal favorito?");
+
+    if (animal.toLowerCase() = tortuga) {
+        alert("También me gustan las tortugas!");
+        preguntas = preguntas +1
+    } else {
+        alert("Ese animal es genial, pero prefiero las tortugas!");
+    }
+}
