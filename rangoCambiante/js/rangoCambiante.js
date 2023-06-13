8f15a4f6a5ddc91fcf9a4d520915e527ef8b6331
Susana Alvarado <130176230+SusAlvarado@users.noreply.github.com>
2023-06-13 11:03:58 -0600

    se añaden files a la carpeta de rango cambiante

---
+++ rangoCambiante/js/rangoCambiante.js
@@ -0,0 +1,4 @@
+/*
+Reto #3 - Rangos cambiantes.
+Nuevamente pide a tu usuario que indique 3 números: un límite superior, un límite inferior y uno de comparación. Si el número de comparación se encuentra entre los 2 primeros, comunicarlo en pantalla. En caso estar por debajo del límite inferior o por arriba del límite superior, también mostrarlo en pantalla.
+*/
